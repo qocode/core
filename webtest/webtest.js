4aaa458aa0d205cf831d7134f3cd3a467bb880b9
IgorNovozhilov <maildrakon@gmail.com>
2020-02-18 23:13:00 +0300

    webtest

--- webtest/webtest.js
+++ webtest/webtest.js
@@ -1,17 +1,17 @@
 import {
-  deflateURLSearchParams, inflateURLSearchParams,
+  deflateJSONURL, inflateJSONURL,
   QOData
 } from './build/qosource.js'
 
 
-console.log(deflateURLSearchParams('!deflateURLSearchParams!'))
+console.log(deflateJSONURL('!deflateJSONURL!'))
 
-console.log(inflateURLSearchParams(deflateURLSearchParams('!inflateURLSearchParams!')))
+console.log(inflateJSONURL(deflateJSONURL('!inflateJSONURL!')))
 
 
 const { URL } = window
-const param1 = deflateURLSearchParams(JSON.stringify({ api: 'qcos.ru' }))
-const param2 = deflateURLSearchParams(JSON.stringify({ price: 100 }))
+const param1 = deflateJSONURL(JSON.stringify({ api: 'qcos.ru' }))
+const param2 = deflateJSONURL(JSON.stringify({ price: 100 }))
 const url = `http://qcos.ru/?name=title&${param1}&${param2}`
 const qodata = new QOData(url)
 
