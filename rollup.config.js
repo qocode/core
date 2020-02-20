ba5d1596dc378630367176fcdb049df3b2fac37a
IgorNovozhilov <maildrakon@gmail.com>
2020-02-21 00:00:43 +0300

    class-properties

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,25 @@
-import alias from '@rollup/plugin-alias'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
 import { terser } from 'rollup-plugin-terser'
 
 export default [{
   input: 'src/pako.js',
-  output: {
-    dir: 'build',
-    format: 'esm',
-    compact: true
-  },
+  output: { file: 'build/tmp/pako.js', format: 'esm', compact: true },
   plugins: [
-    resolve({
-      browser: true,
-      preferBuiltins: false
-    }),
-    commonjs({
-      include: 'node_modules/**'
-    }),
-    cleanup({
-      comments: 'none'
-    }),
+    resolve({ browser: true, preferBuiltins: false }),
+    commonjs({ include: 'node_modules/**' }),
+    cleanup({ comments: 'none' }),
     terser()
   ]
 }, {
   input: 'src/qosource.js',
-  external: ['./external.js'],
-  output: {
-    dir: 'build',
-    format: 'esm'
-  },
-  plugins: [
-    alias({
-      entries: [
-        { find: './pako.js', replacement: './build/pako.js' }
-      ]
-    }),
-    cleanup({
-      comments: 'none'
-    })
-  ]
+  external: ['./pako.js'],
+  output: { file: 'build/tmp/qosource.js', format: 'esm' },
+  plugins: [babel({ plugins: ['@babel/plugin-proposal-class-properties'] })]
+}, {
+  input: 'build/tmp/qosource.js',
+  output: { file: 'build/qosource.js', format: 'esm' },
+  plugins: [cleanup({ comments: 'none' })]
 }]
