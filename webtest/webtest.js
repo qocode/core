31960234c9b0fe4389e2c323e595e7b351349789
IgorNovozhilov <maildrakon@gmail.com>
2020-03-02 21:21:25 +0300

    QRCode.toCanvas

--- webtest/webtest.js
+++ webtest/webtest.js
@@ -2,7 +2,7 @@ import {
   deflateJSONURL, inflateJSONURL,
   QOData
 } from './build/qosource.js'
-
+import { QRCode } from './build/external.js'
 
 console.log(deflateJSONURL({ test: 'test' }))
 
@@ -20,3 +20,30 @@ qodata.update(new URL('http://qcos.ru/?name=тест&='))
 qodata.update({ price: 1000 })
 console.log(qodata.raw)
 console.log(qodata.stringify())
+
+
+const conf1 = { color: { light: '#ffffff00' }, errorCorrectionLevel: 'H', margin: 4, scale: 1 }
+const conf2 = { color: { light: '#ffffff00' }, errorCorrectionLevel: 'H', margin: 3, scale: 2 }
+const conf3 = { color: { light: '#ffffff00' }, errorCorrectionLevel: 'H', margin: 2, scale: 3 }
+
+
+QRCode.toCanvas(document.getElementById('canvas_1'), 'qcos.ru', conf1,
+  error => { if (error) console.error(error) })
+QRCode.toCanvas(document.getElementById('canvas_2'), 'qcos.ru', conf2,
+  error => { if (error) console.error(error) })
+QRCode.toCanvas(document.getElementById('canvas_3'), 'qcos.ru', conf3,
+  error => { if (error) console.error(error) })
+
+QRCode.toCanvas(document.getElementById('canvas_4'), 'http://qcos.ru', conf1,
+  error => { if (error) console.error(error) })
+QRCode.toCanvas(document.getElementById('canvas_5'), 'http://qcos.ru', conf2,
+  error => { if (error) console.error(error) })
+QRCode.toCanvas(document.getElementById('canvas_6'), 'http://qcos.ru', conf3,
+  error => { if (error) console.error(error) })
+
+QRCode.toCanvas(document.getElementById('canvas_7'), 'https://qcos.ru', conf1,
+  error => { if (error) console.error(error) })
+QRCode.toCanvas(document.getElementById('canvas_8'), 'https://qcos.ru', conf2,
+  error => { if (error) console.error(error) })
+QRCode.toCanvas(document.getElementById('canvas_9'), 'https://qcos.ru', conf3,
+  error => { if (error) console.error(error) })
